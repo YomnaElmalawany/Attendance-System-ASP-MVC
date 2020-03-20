c74897a22fc81909a258ed216417604b8f61caf5
YomnaElmalawany <32571876+YomnaElmalawany@users.noreply.github.com>
2020-03-20 17:08:21 +0200

    AdminView3_StudentViews_v2

---
+++ mvc_project/mvc_project/Models/Attendance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mvc_project.Models
+{
+    public class Attendance
+    {
+        [Key]
+        public int AttId { get; set; }
+        [ForeignKey("User")]
+        public string StdId { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public string AttDate { get; set; }
+        public string TimeAttend { get; set; }
+        public string TimeLeft { get; set; }
+    }
+}
---
+++ mvc_project/mvc_project/Models/Permission.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mvc_project.Models
+{
+    public class Permission
+    {
+        [Key]
+        public int PermId { set; get; }
+        [ForeignKey("User")]
+        public string StdId { get; set; }
+        public ApplicationUser User { get; set; }
+        public string PermDate { get; set; }
+        public string Note { get; set; }
+        public string Status { get; set; }
+    }
+}
